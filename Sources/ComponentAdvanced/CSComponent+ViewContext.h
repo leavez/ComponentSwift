5755b7155eb3e0208935130a96328ecf5b32d22e
leavez <gaojiji@gmail.com>
2017-06-18 16:31:43 +0800

    set swift name for getViewFromCKComponent

--- Sources/ComponentAdvanced/CSComponent+ViewContext.h
+++ Sources/ComponentAdvanced/CSComponent+ViewContext.h
@@ -12,7 +12,7 @@
 @interface CSComponent(ViewContext)
 
 /// Get the mounted view of ckcomponent
-/// maybe used in the call back parameter of tap action
-+ (nullable UIView *)getViewFromCKComponent:(nullable id)component;
+/// maybe used in the parameter of gesture call back
++ (nullable UIView *)getViewFromCKComponent:(nullable id)component NS_SWIFT_NAME(getView(fromCKComponent:));
 
 @end
